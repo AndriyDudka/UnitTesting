ce3eb273b951a4257a7c33cfc026c1a2634be3d6
Dudka <andriy.dudka@gmail.com>
2016-07-19 21:23:14 +0300

    add unit tests

---
+++ Logic.Tests/AlgoServiceTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Logic;
+using NUnit.Framework;
+
+namespace ForTest
+{
+    [TestFixture]
+    public class AlgoServiceTests
+    {
+        private IAlgoService _underTest;
+
+        #region DoubleSumTests
+
+        [Test]
+        public void DoubleSumNullArgThrowsExceptionWhenArgIsNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => _underTest.DoubleSum(null));
+        }
+
+        [Test]
+        public void DoubleSumReturnsNullWhenArgIsEmptyTest()
+        {
+            var empty = new List<int>();
+
+            var result = _underTest.DoubleSum(empty);
+
+            Assert.That(_underTest.MethodsCalledCount, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void DoubleSumHappyPathTest()
+        {
+            var digits = new List<int> { -1, 1, -2, 2, 0};
+            const int expected = 0;
+
+            var result = _underTest.DoubleSum(digits);
+
+            Assert.That(_underTest.MethodsCalledCount, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        #endregion
+
+        #region MinValue
+
+        [Test]
+        public void GetMinValueThrowsInvalidOperationExceptionWhenDataIsNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => _underTest.MinValue(null));
+        }
+
+        [Test]
+        public void GetMinValueThrowsInvalidOperationExceptionWhenDataIsEmptyTest()
+        {
+            var empty = new List<int>();
+
+            Assert.Throws<InvalidOperationException>(() => _underTest.MinValue(empty));
+        }
+
+        [Test]
+        public void GetMinValueHappyPathTest()
+        {
+            var digits = new List<int> { -1, 1, -2, 2, 0 };
+            const int expected = -2;
+
+            var result = _underTest.MinValue(digits);
+      
+            Assert.AreEqual(result, expected);
+        }
+
+        #endregion
+
+        #region Function
+        //Не враховано, що від відємного числа немає корня(не рахуючи комплексні числа)
+        [Test]
+        public void FunctionThrowExceptionTest()
+        {
+            Assert.Throws<InvalidOperationException>(() => _underTest.Function(1, -1, 1, 1));
+        }
+
+        [Test]
+        public void FunctionWhenAllIsOkTest()
+        {
+            var function = _underTest.Function(1, 1, 1, 1);
+            var expected = 2 - Math.PI;
+
+            Assert.That(function, Is.EqualTo(expected));
+        }
+
+        #endregion
+
+        #region GetAverage
+
+        [Test]
+        public void GetAverageNullArgThrowsExceptionWhenArgIsNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => _underTest.GetAverage(null));
+        }
+    
+        [Test]
+        public void GetAverageHappyPathTest()
+        {
+            var digits = new List<int> {1, 2, 3};
+            const int expected = 2;
+
+            var result = _underTest.GetAverage(digits);
+     
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        #endregion
+
+        #region GetSqr
+       
+        [Test]
+        public void GetSqrHappyPathTest()
+        {
+            var result = _underTest.Sqr(2);
+            var expected = 4;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        #endregion
+
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _underTest = new AlgoService();
+        }
+
+        #endregion
+    }
+}
---
+++ Logic.Tests/MasterServiceTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+using Moq;
+using NUnit.Framework;
+
+namespace ForTest
+{
+    [TestFixture]
+    public class MasterServiceTests
+    {
+        private Mock<IAlgoService> _algoServiceMock;
+
+        private Mock<IDataService> _dataServicMock;
+
+        private IMasterService _underTest;
+
+
+        #region GetDoubleSum
+
+        [Test]
+        public void GetDoubleSumThrowsInvalidOperationExceptionWhenDataIsNullTest()
+        {
+            _dataServicMock.Setup(ds => ds.GetAllData()).Returns(null as IEnumerable<int>);
+
+            Assert.Throws<InvalidOperationException>(() =>_underTest.GetDoubleSum());
+        }
+
+        [Test]
+        public void GetDoubleSumThrowsInvalidOperationExceptionWhenDataIsEmptyTest()
+        {
+            _dataServicMock.Setup(ds => ds.GetAllData()).Returns(new List<int>());
+
+            Assert.Throws<InvalidOperationException>(() => _underTest.GetDoubleSum());
+        }
+
+        [Test]
+        public void GetDoubleSumDoesCallToAlgoServiceDoubleSumWhenAllIsOkTest()
+        {
+            var data = new List<int> {1,2,3};
+            _dataServicMock.Setup(ds => ds.GetAllData()).Returns(data);
+
+            _underTest.GetDoubleSum();
+
+            _algoServiceMock.Verify(algs => algs.DoubleSum(data));
+        }
+
+        #endregion
+
+        #region GetAverage
+
+        [Test]
+        public void GetAverageDoesCallToAlgoServiceAverageTest()
+        {
+            var data = new List<int> { 1, 2, 3 };
+            _dataServicMock.Setup(ds => ds.GetAllData()).Returns(data);
+
+            _underTest.GetAverage();
+
+            _algoServiceMock.Verify(algs => algs.GetAverage(data));
+        }
+
+        #endregion
+
+        #region GetMaxSquare
+
+        [Test]
+        public void GetMaxSquareDoesCallToAlgoServiceGetMaxSquareTest()
+        {
+            var data = 20;
+            _dataServicMock.Setup(ds => ds.GetMax()).Returns(data);
+
+            _underTest.GetMaxSquare();
+
+            _algoServiceMock.Verify(algs => algs.Sqr(data));
+        }
+        #endregion
+
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algoServiceMock = new Mock<IAlgoService>();
+            _dataServicMock = new Mock<IDataService>();
+            
+            _underTest = new MasterService(_algoServiceMock.Object, _dataServicMock.Object);
+        }
+
+        #endregion
+
+    }
+}
